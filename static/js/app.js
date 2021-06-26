d513c61da67b62a35c3f0343acb6a411b99fd151
gldn-god <78330405+gldn-god@users.noreply.github.com>
2021-06-26 16:06:26 -0600

    slimmed code down

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,12 @@
-// initially populate menu with IDs, draw charts using first record by default 
+// populate menu with IDs, draw charts using first record by default 
 function init() {
     resetData();
-    // read in data using D3 library
     d3.json("data/samples.json").then((data => {
-        //  populate dropdowns with IDs
         data.names.forEach((name => {
             var option = idSelect.append("option");
             option.text(name);
         }));
-        // grab first ID as default
         var initId = idSelect.property("value")
-        // plot initial ID
         plotCharts(initId);
     }));
 }
