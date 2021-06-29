b60b2f39120c2c920fe8c113f1ee4c6796998a3f
gldn-god <78330405+gldn-god@users.noreply.github.com>
2021-06-28 21:59:16 -0600

    brought log prints back in... reference breaks for barchart

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,14 @@
-// populate dropdown menu with options for user input
+// create metadata for dropdown menus
 function buildMetadata(selection) {
     d3.json("samples.json").then((sampleData) => {
         console.log(sampleData);
 
         var parsedData = sampleData.metadata;
+        console.log("parsed data")
         console.log(parsedData);
 
         var sample = parsedData.filter(item => item.id == selection);
+        console.log("sample:");
         console.log(sample[0]);
 
         var metadata = d3.select("#sample-metadata").html("");
@@ -14,7 +16,8 @@ function buildMetadata(selection) {
         Object.entries(sample[0]).forEach(([key, value]) => {
             metadata.append("p").text(`${key}: ${value}`);
         });
-        // record metadata
+        // show function is looping in console
+        console.log("repeat");
         console.log(metadata);
     });
 }
@@ -25,26 +28,32 @@ function buildCharts(selection) {
     d3.json("samples.json").then((sampleData) => {
 
         var parsedData = sampleData.samples;
+        console.log("parsed data")
         console.log(parsedData);
 
         var sampleDict = parsedData.filter(item => item.id == selection)[0];
+        console.log("sampleDict")
         console.log(sampleDict);
 
         var sampleValues = sampleDict.sample_values; 
         var barChartValues = sampleValues.slice(0, 10).reverse();
+        console.log("sample_values")
         console.log(barChartValues);
 
         var idValues = sampleDict.otu_ids;
         var barChartLabels = idValues.slice(0, 10).reverse();
+        console.log("otu_ids");
         console.log(barChartLabels);
 
         var reformattedLabels = [];
         barChartLabels.forEach((label) => {
         });
+        console.log("reformatted");
         console.log(reformattedLabels);
 
         var hovertext = sampleDict.otu_labels;
         var barCharthovertext = hovertext.slice(0, 10).reverse();
+        console.log("otu_labels");
         console.log(barCharthovertext);
 
         // create barchart
@@ -72,7 +81,7 @@ function buildCharts(selection) {
         };
         var bubbleChartData = [bubbleChartTrace];
         var layout = {
-            showlegend: true,
+            showlegend: false,
             height: 500,
             width: 800,
             xaxis: {
@@ -87,6 +96,7 @@ function buildCharts(selection) {
 function init() {
     d3.json("samples.json").then((data) => {
         var parsed = data.names;
+        console.log("parsed data")
         console.log(parsed);
 
         // create dropwon menu from each selectable value
